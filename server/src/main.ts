7a6e1d6c3ebf894c9fd74c9bc79a2f6299416e38
Miguel Estrada López <miki@zonesport.local>
2026-03-05 14:19:28 -0500

    Enable implicit conversion in global ValidationPipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -90,8 +90,10 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: false,
       transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
 
